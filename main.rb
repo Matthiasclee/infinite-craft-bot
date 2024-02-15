25e61953f509dc2f3575744fba32a364a835f402
Matthias Lee <matthias@matthiasclee.com>
2024-02-14 17:16:55 -0800

    Misc

--- main.rb
+++ main.rb
@@ -1,9 +1,10 @@
-require "net/http"
-require "uri"
 require "json"
 
-API_PAIR_URL = "https://neal.fun/api/infinite-craft/pair"
-
 def check_pair(item_1, item_2)
-  uri = URI.parse("#{API_PAIR_URL}?first=#{item_1}&second=#{item_2}")
+  url = "https://neal.fun/api/infinite-craft/pair?first=#{item_1}&second=#{item_2}"
+  curl_command = "curl -H 'Referer: https://neal.fun/infinite-craft/' '#{url}'"
+  item_info = JSON.parse `#{curl_command}`
+  return item_info
 end
+
+puts check_pair("Water", "Earth").to_s
