cc44617054c565ba6f7dfe0d69567c722acbde85
Matthias Lee <matthias@matthiasclee.com>
2024-02-14 19:10:52 -0800

    Misc

--- main.rb
+++ main.rb
@@ -7,7 +7,11 @@ def check_pair(item_1, item_2)
   curl_command = "curl -s -H 'Referer: https://neal.fun/infinite-craft/' '#{url}'"
   response = `#{curl_command}`
   return false if response.include?("error code: 1015")
-  item_info = JSON.parse response 
+  begin
+    item_info = JSON.parse response 
+  rescue
+    return false
+  end
   return item_info
 end
 
@@ -15,27 +19,39 @@ def print_item(item)
   "#{item[0]}"
 end
 
-items.each do |item|
-  items.each do |item_1|
-    result = nil
-    loop do
-      result = check_pair(item[0], item_1[0])
-      break if result
-      puts "Ratelimited. Waiting 20 minutes..."
-      1200.times do |x|
-        print "\r#{x}/1200 seconds"
-        sleep 1
+begin
+  items.each do |item|
+    items.each do |item_1|
+      result = nil
+      loop do
+        result_1 = check_pair(item[0], item_1[0])
+        break if result_1
+        puts "Something went wrong, you are most likely being ratelimited. Trying again in 20 minutes..."
+        1200.times do |x|
+          print "\r#{x}/1200 seconds"
+          sleep 1
+        end
+        puts
       end
-      puts
+      result = [result_1["result"], result_1["emoji"]]
+      unless items.include?(result)
+        items << result 
+      end
+      recipe = "#{print_item item} + #{print_item item_1} = #{print_item result}"
+      puts recipe
+      File.write("recipes.ignore", recipe, mode: ?a)
+      if result_1["isNew"]
+        puts "#{print_item result} is a new discovery!"
+        File.write("discoveries.ignore", recipe, mode: ?a)
+      end
+      sleep(5) # no ratelimit
     end
-    result = [result["result"], result["emoji"]]
-    unless items.include?(result)
-      items << result 
-    end
-    puts "#{print_item item} + #{print_item item_1} = #{print_item result}"
-    sleep(5) # no ratelimit
-  end
 
-  puts "Sleeping for 10 seconds to prevent ratelimit"
-  sleep(10) # no ratelimit
+    puts "Sleeping for 10 seconds to prevent ratelimit"
+    sleep(10) # no ratelimit
+  end
+rescue Interrupt
+  puts "Interrupted, continuing..."
 end
+
+
